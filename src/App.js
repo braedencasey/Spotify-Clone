25dd9b734caef2f67ab977d7c236104deb5d649c
Braeden <braedencasey4@gmail.com>
2023-01-15 10:06:37 -0600

    Integrated Spotify API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import './App.css';
-import Login from'./Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  //Run's code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        
+      })
+    }
+  }, []);
+
   return (
-    <div className = "app">
+    <div className="app">
+      {token ? 
+      <Player /> : <Login />}
       <Login />
     </div>
-   
- 
   );
 }
 
