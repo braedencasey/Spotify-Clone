32f59d9df7abceeac4289077f712b70b57c2e7a9
Braeden <braedencasey4@gmail.com>
2023-01-16 15:59:18 -0600

    Styled Home Page and Side Bar

--- src/App.js
+++ src/App.js
@@ -3,33 +3,43 @@ import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { DataLayer, useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  
 
-  //Run's code based on a given condition
+  const [{ user, token }, dispatch] = useDataLayerValue();
+ 
+
+  //Runs code based on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
 
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user => {
-        
-      })
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
   }, []);
 
   return (
     <div className="app">
-      {token ? 
-      <Player /> : <Login />}
+      {token ? <Player spotify = {spotify} /> : <Login />}
       <Login />
     </div>
   );
